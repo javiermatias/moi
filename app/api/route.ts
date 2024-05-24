f515d036131a8fafa222d4653863b482d3dc80d5
Javier Matias <javierjimenez78@gmail.com>
2024-05-24 01:55:02 -0300

    add mysql

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,49 @@
 import { type NextRequest } from 'next/server'
+import executeQuery from '../lib/db';
+
 
 export async function GET(request: NextRequest) {
   const token = request.cookies.get('token')
+  try {
+    //console.log("req nom", req.body)
+    const result = await executeQuery({
+      query: 'INSERT INTO bitacora (fecha_hora, asunto, lugar, convocado_por) VALUES (?, ?, ?, ?)',
+      values: [
+        '2024-05-23 14:30:00',  // fecha_hora
+        'Reunión de Proyecto',    // asunto
+        'Sala de Conferencias',   // lugar
+        'Juan Pérez'              // convocado_por
+      ]
+    });
+    console.log("ttt", result);
+  } catch (error) {
+    console.log(error);
+  }
   return new Response(`Hello, Next.js! token=${token?.value}`, {
     status: 200,
     headers: { 'Set-Cookie': `token=${token?.value}` },
   })
+}
+
+export async function POST(request: NextRequest) {
+  const { fecha_hora, asunto, lugar, convocado_por } = await request.json();
+  try {
+    //console.log("req nom", req.body)
+    const result = await executeQuery({
+      query: 'INSERT INTO bitacora (fecha_hora, asunto, lugar, convocado_por) VALUES (?, ?, ?, ?)',
+      values: [
+        '2024-05-23 14:30:00',  // fecha_hora
+        'Reunión de Proyecto',    // asunto
+        'Sala de Conferencias',   // lugar
+        'Juan Pérez'              // convocado_por
+      ]
+    });
+    return new Response('Bitacora created sucesfully')
+  } catch (error) {
+    console.log(error);
+  }
+  /*   return new Response(`Hello, Next.js! token=${token?.value}`, {
+      status: 200,
+      headers: { 'Set-Cookie': `token=${token?.value}` },
+    }) */
 }
--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,15 +1,20 @@
-// db.js
 import mysql from 'serverless-mysql';
+
+interface QueryProps {
+    query: string;
+    values: any[];
+}
+const port = parseInt(process.env.MYSQL_PORT || '3306', 10);
 const db = mysql({
     config: {
         host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
+        port: port,
         database: process.env.MYSQL_DATABASE,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD
     }
 });
-export default async function excuteQuery({ query, values }) {
+export default async function executeQuery({ query, values }: QueryProps) {
     try {
         const results = await db.query(query, values);
         await db.end();
